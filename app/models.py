35c152ab2f01390888ee1d4f181fedf16ac86095
Rick Flagg <Richard.Lee.Flagg@outlook.com>
2022-03-27 13:25:49 -0400

    Integrated api security

---
+++ app/models.py
@@ -0,0 +1,46 @@
+from typing import Optional, List, Dict, Any
+from pydantic import BaseModel
+from fastapi_users import models
+
+
+class User(models.BaseUser):
+    pass
+
+
+class UserCreate(models.BaseUserCreate):
+    pass
+
+
+class UserUpdate(models.BaseUserUpdate):
+    pass
+
+
+class UserDB(User, models.BaseUserDB):
+    pass
+
+
+class CheckAttribute(BaseModel):
+    Run: bool
+    Threshold: Optional[int] = None
+    TimeStart: Optional[str] = None
+    TimeEnd: Optional[str] = None
+    EventTypeValue: Optional[str] = None
+
+
+class AnalyzerConfiguration(BaseModel):
+    RequestHeaderLogField: str
+    UserIdLogField: str
+    EventTypeLogField: str
+    EventDateLogField: str
+    RemoteAddressLogField: str
+    SqlInjectionCheck: CheckAttribute
+    ExcessiveEventCheck: CheckAttribute
+    CrossFrameScriptingCheck: CheckAttribute
+    LDAPInjectionCheck: CheckAttribute
+    ExcessiveLoginCheck: CheckAttribute
+    ExcessivePasswordChangeCheck: CheckAttribute
+    LoginAfterHoursCheck: CheckAttribute
+    ReportWebInspect: bool
+    ReportNessus: bool
+    ReportCrossFeed: bool
+    Logs: List[Dict[Any, Any]]
--- main.py
+++ main.py
@@ -1,43 +1,4 @@
-from fastapi import FastAPI
-from datetime import time
-from typing import Optional, List, Dict, Any
-from pydantic import BaseModel
+import uvicorn
 
-app = FastAPI()
-
-
-class CheckAttribute(BaseModel):
-    Run: bool
-    Threshold: Optional[int] = None
-    TimeStart: Optional[str] = None
-    TimeEnd: Optional[str] = None
-    EventTypeValue: Optional[str] = None
-
-
-class AnalyzerConfiguration(BaseModel):
-    RequestHeaderLogField: str
-    UserIdLogField: str
-    EventTypeLogField: str
-    EventDateLogField: str
-    RemoteAddressLogField: str
-    SqlInjectionCheck: CheckAttribute
-    ExcessiveEventCheck: CheckAttribute
-    CrossFrameScriptingCheck: CheckAttribute
-    LDAPInjectionCheck: CheckAttribute
-    ExcessiveLoginCheck: CheckAttribute
-    ExcessivePasswordChangeCheck: CheckAttribute
-    LoginAfterHoursCheck: CheckAttribute
-    ReportWebInspect: bool
-    ReportNessus: bool
-    ReportCrossFeed: bool
-    Logs: List[Dict[Any, Any]]
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.post("/analyze")
-async def analyze(conf: AnalyzerConfiguration):
-    return  {"message": "Ok"}
+if __name__ == "__main__":
+    uvicorn.run("app.app:app", log_level="info", port=8080)
